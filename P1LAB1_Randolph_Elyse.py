f89e16546013f7fcabefc9e123b5040fc14de047
Elyse-R <randolpa1120@student.faytechcc.edu>
2024-09-21 19:31:16 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_Randolph_Elyse.py
@@ -0,0 +1,8 @@
+# Elyse Randolph
+# September 21 2024
+# P1LAB1
+# Prgram ask for users first and last names and prints a welcome message using the inputs
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+print("Hello,", firstName,lastName + "! Welcome to CTI-110")
